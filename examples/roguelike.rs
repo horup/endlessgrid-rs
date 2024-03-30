287563a9afaa8f4ffaeb6297a308c90d5be9402e
= <horup@deepstacker.com>
2024-03-30 12:49:16 +0100

    wip

--- examples/roguelike.rs
+++ examples/roguelike.rs
@@ -1,16 +1,24 @@
 use std::collections::HashMap;
 
 use egrid::*;
-use macroquad::{miniquad::MipmapFilterMode, prelude::*};
+use macroquad::prelude::*;
+use slotmap::{DefaultKey, SlotMap};
 use tiled::Loader;
 #[derive(Default, Clone)]
 struct Tile {
     pub index:u16,
     pub blocks_los: bool,
+    pub entities:HashMap<DefaultKey, ()>
+}
+
+#[derive(Default, Clone)]
+pub struct Entity {
+    pub pos:(i32, i32),
+    pub is_player:bool
 }
 
 
-fn load_map(grid:&mut EGrid<Tile>) {
+fn load_map(grid:&mut EGrid<Tile>, entities:&mut SlotMap<DefaultKey, Entity>) {
     let mut loader = Loader::new();
     let map = loader.load_tmx_map("examples/roguelike.tmx").unwrap();
     for layer in map.layers() {
@@ -31,9 +39,19 @@ fn load_map(grid:&mut EGrid<Tile>) {
                         let classes = tile.get_tile().unwrap().user_type.clone().unwrap_or_default();
                         let classes = classes.split(" ").map(|x|(x.to_owned(), ()));
                         let classes:HashMap<String,()> = classes.collect();
+                        let mut keys = HashMap::default();
+                        if classes.contains_key("player") {
+                            let key = entities.insert(Entity {
+                                pos:tile_pos,
+                                is_player:true
+                            });
+                            keys.insert(key, ());
+                        }
+                         
                         grid.insert(tile_pos, Tile {
                             index:tile.id() as u16,
-                            blocks_los:classes.contains_key("solid")
+                            blocks_los:classes.contains_key("solid"),
+                            entities:keys
                         });
                     }
                 }
@@ -58,10 +76,20 @@ fn draw_atlas(texture:&Texture2D, x:f32, y:f32, index:f32, color:Color, tile_siz
     });
 }
 
+fn move_player(grid:&mut EGrid<Tile>, from:(i32, i32), to:(i32, i32)) -> (i32, i32) {
+    let mut current = from;
+    if from != to {
+        //let player = grid.get_mut(from).and_then(|x| x.entities.iter_mut().filter(|x|x.is_player).next());
+        let v = Vec::default() as Vec<f32>;
+    }
+    current
+}
+
 #[macroquad::main("Roguelike")]
 async fn main() {
+    let mut entities = SlotMap::default();
     let mut grid = EGrid::default() as EGrid<Tile>;
-    load_map(&mut grid);
+    load_map(&mut grid, &mut entities);
     let tile_size_px = 8.0;
     let tilemap_texture = load_texture("examples/tileset.png").await.unwrap();
     tilemap_texture.set_filter(FilterMode::Nearest);
@@ -80,34 +108,12 @@ async fn main() {
         for y in (player_pos.1 - view_distance)..(player_pos.1 + view_distance) {
             for x in (player_pos.0 - view_distance)..(player_pos.0 + view_distance) {
                 if let Some(tile) = grid.get((x, y)) {
-                    /*draw_rectangle(
-                        x as f32 * tile_size_px,
-                        y as f32 * tile_size_px,
-                        tile_size_px as f32,
-                        tile_size_px as f32,
-                        color,
-                    );*/
                     let x = x as f32 * tile_size_px;
                     let y = y as f32 * tile_size_px;
                     draw_atlas(&tilemap_texture, x, y, tile.index as f32, WHITE, tile_size_px);
                 }
             }
         }
-
-        // draw player
-        draw_circle(
-            player_pos.0 as f32 * tile_size_px as f32 + tile_size_px as f32 / 2.0,
-            player_pos.1 as f32 * tile_size_px as f32 + tile_size_px as f32 / 2.0,
-            tile_size_px as f32 / 2.0,
-            WHITE,
-        );
-
-        /*draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-                draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-                draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-                draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
-        */
         next_frame().await
     }
 }

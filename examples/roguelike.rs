789b04d9accc257cdb00a02cb2b745d44145cddb
= <horup@deepstacker.com>
2024-03-30 13:08:04 +0100

    wip

--- examples/roguelike.rs
+++ examples/roguelike.rs
@@ -7,13 +7,14 @@ use tiled::Loader;
 #[derive(Default, Clone)]
 struct Tile {
     pub index:u16,
-    pub blocks_los: bool,
+    pub solid: bool,
     pub entities:HashMap<DefaultKey, ()>
 }
 
 #[derive(Default, Clone)]
 pub struct Entity {
-    pub pos:(i32, i32),
+    pub pos:IVec2,
+    pub index:u16,
     pub is_player:bool
 }
 
@@ -42,15 +43,16 @@ fn load_map(grid:&mut EGrid<Tile>, entities:&mut SlotMap<DefaultKey, Entity>) {
                         let mut keys = HashMap::default();
                         if classes.contains_key("player") {
                             let key = entities.insert(Entity {
-                                pos:tile_pos,
+                                index:tile.id() as u16,
+                                pos:tile_pos.into(),
                                 is_player:true
                             });
                             keys.insert(key, ());
                         }
                          
                         grid.insert(tile_pos, Tile {
-                            index:tile.id() as u16,
-                            blocks_los:classes.contains_key("solid"),
+                            index:if classes.contains_key("entity") { 0 } else { tile.id() as u16 },
+                            solid:classes.contains_key("solid"),
                             entities:keys
                         });
                     }
@@ -76,13 +78,40 @@ fn draw_atlas(texture:&Texture2D, x:f32, y:f32, index:f32, color:Color, tile_siz
     });
 }
 
-fn move_player(grid:&mut EGrid<Tile>, from:(i32, i32), to:(i32, i32)) -> (i32, i32) {
-    let mut current = from;
-    if from != to {
-        //let player = grid.get_mut(from).and_then(|x| x.entities.iter_mut().filter(|x|x.is_player).next());
-        let v = Vec::default() as Vec<f32>;
+fn move_entity(entity:DefaultKey, to:(i32, i32), grid:&mut EGrid<Tile>, entities:&mut SlotMap<DefaultKey, Entity>) {
+    
+    let entity_key = entity;
+    let Some(entity) = entities.get_mut(entity_key) else { return };
+    let Some(to_tile) = grid.get_mut(to) else { return };
+
+    if to_tile.solid { return };
+    let org_pos = entity.pos;
+    to_tile.entities.insert(entity_key, ());
+    let Some(from_tile) = grid.get_mut((org_pos.x, org_pos.y)) else { return };
+    from_tile.entities.remove(&entity_key);
+    entity.pos = to.into();
+
+}
+
+fn input(player_entity:DefaultKey, grid:&mut EGrid<Tile>, entities:&mut SlotMap<DefaultKey, Entity>) {
+    let Some(entity) = entities.get(player_entity) else { return };
+    let mut pos = entity.pos;
+    if is_key_pressed(KeyCode::W) {
+        pos.y -= 1;
     }
-    current
+    if is_key_pressed(KeyCode::S) {
+        pos.y += 1;
+    }
+    if is_key_pressed(KeyCode::A) {
+        pos.x -= 1;
+    }
+    if is_key_pressed(KeyCode::D) {
+        pos.x += 1;
+    }
+
+    if pos != entity.pos {
+        move_entity(player_entity, (pos.x, pos.y), grid, entities);
+    }
 }
 
 #[macroquad::main("Roguelike")]
@@ -90,10 +119,12 @@ async fn main() {
     let mut entities = SlotMap::default();
     let mut grid = EGrid::default() as EGrid<Tile>;
     load_map(&mut grid, &mut entities);
+    let player_entity = entities.iter().filter(|x|x.1.is_player).next().expect("player not ofund").0;
     let tile_size_px = 8.0;
     let tilemap_texture = load_texture("examples/tileset.png").await.unwrap();
     tilemap_texture.set_filter(FilterMode::Nearest);
     loop {
+        input(player_entity, &mut grid, &mut entities);
         let zoom = 4.0;
         let camera = Camera2D {
             zoom:Vec2::new(zoom / screen_width(), zoom / screen_height()),
@@ -111,6 +142,10 @@ async fn main() {
                     let x = x as f32 * tile_size_px;
                     let y = y as f32 * tile_size_px;
                     draw_atlas(&tilemap_texture, x, y, tile.index as f32, WHITE, tile_size_px);
+                    for entity in tile.entities.keys() {
+                        let Some(entity) = entities.get(*entity) else { continue;};
+                        draw_atlas(&tilemap_texture, x, y, entity.index as f32, WHITE, tile_size_px);
+                    }
                 }
             }
         }
